7ce08ad5c2792eafd500f18bbf56133f128dbe55
kadeksuryaindrawan <kadeksuryaindrawan@gmail.com>
2022-12-27 00:50:19 +0800

    :smile: new-dokter

--- process/rekammedis/tambah_rekammedis.php
+++ process/rekammedis/tambah_rekammedis.php
@@ -1,13 +1,20 @@
 <?php
     require_once "../../config/connection.php";
-    
+    session_start();
     extract($_POST);
-var_dump(extract($_POST));  
 
-session_start();
     $level = $_SESSION['user_login']['level'];
-
-    $query_insert = mysqli_query($connection, "INSERT INTO tbrekammedis VALUES (NULL, '$id_dokter', '$nik', '$sakit', '$jenis_penyakit', '$pemeriksaan','$alergi_obat','$pengobatan','$lainnya', now())");
+    if($level == 'admin'){
+        $query_insert = mysqli_query($connection, "INSERT INTO tbrekammedis VALUES (NULL, '$id_dokter', '$nik', '$sakit', '$jenis_penyakit', '$pemeriksaan','$alergi_obat','$pengobatan','$lainnya', now())");
+    }
+    else{
+        $id = $_SESSION['user_login']['id_dokter'];
+        $query_dokter = mysqli_query($connection,"SELECT * FROM tbdokter WHERE id_user = '$id'");
+        $data_dokter = mysqli_fetch_assoc($query_dokter);
+        $id_dok = $data_dokter['id_dokter'];
+        $query_insert = mysqli_query($connection, "INSERT INTO tbrekammedis VALUES (NULL, '$id_dok', '$nik', '$sakit', '$jenis_penyakit', '$pemeriksaan','$alergi_obat','$pengobatan','$lainnya', now())");
+    }
+    
     if($query_insert){
         if($level == 'admin'){
             header("location:../../app/admin/lihatrekam.php?nik=$nik");
